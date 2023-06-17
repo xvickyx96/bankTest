9d0997d25b804774d57a53b7eaf8cf4a938c7e00
xvickyx96 <96viksin@gafe.molndal.se>
2023-06-17 18:11:06 +0200

    uppdaterad

--- tests/AccountTests.java
+++ tests/AccountTests.java
@@ -5,20 +5,30 @@ import bank.Account;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AccountTests {
+    private Account account;
+
+    @BeforeEach
+    public void beforeTest() {
+        account = new Account(100.0, "13579");
+    }
+
+    @AfterEach
+    public void afterTest() {
+        account = new Account(100.0, "13579");
+    }
     @Test
     public void testValidDeposit() {
-        Account account = new Account(100.0, "13579");
         account.deposit(50.0);
         assertEquals(150.0, account.getBalance());
     }
     @ParameterizedTest
     @ValueSource(doubles = {0.0, -50.0})
     public void testInvalidDeposit(double amount) {
-        Account account = new Account(100.0, "13579");
         account.deposit(amount);
         assertEquals(100.0, account.getBalance());
     }
@@ -36,7 +46,6 @@ public class AccountTests {
     @ParameterizedTest
     @ValueSource(doubles = {150.0, 0.0, -50.0})
     public void testWithdrawInvalid(double amount) {
-        Account account = new Account(100.0, "13579");
         account.withdraw(amount);
         assertEquals(100.0, account.getBalance());
     }
@@ -45,13 +54,11 @@ public class AccountTests {
 
     @Test
     public void testGetBalance() {
-        Account account = new Account(100.0, "13579");
         assertEquals(100.0, account.getBalance());
     }
 
     @Test
     public void testGetAccountNr() {
-        Account account = new Account(100.0, "13579");
         assertEquals("13579", account.getAccountNr());
     }
 }
